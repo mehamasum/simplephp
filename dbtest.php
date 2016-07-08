5819580ced4625ff6b56a211dcfb74f926903df3
Mehedi Hasan Masum <Mehedi Hasan Masum>
2016-07-08 15:26:20 +0600

    db debug

--- dbtest.php
+++ dbtest.php
@@ -21,11 +21,49 @@ if ($connection->connect_errno) {
     exit();
 }
 echo "<br><br><br><br><h1>My awesome PHP test</h1>";
-$query = "SELECT * from users";
-$rs = $connection->query($query);
-while ($row = $rs->fetch_array(MYSQLI_ASSOC)) {
-    echo $row['user_id'] . " " . $row['username'] . "\n";
+
+
+// create table
+$sql = "CREATE TABLE users (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+username VARCHAR(30) NOT NULL,
+password VARCHAR(50) NOT NULL,
+reg_date TIMESTAMP
+)";
+
+if ($connection->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
 }
+
+// insert values
+$sql = "INSERT INTO users (username, password)
+VALUES ('mehamasum', 'secretpassword')";
+$sql .= "INSERT INTO users (username, password)
+VALUES ('Mary', 'Moe');";
+$sql .= "INSERT INTO users (username, password)
+VALUES ('Julie', 'Dooley')";
+
+if ($conn->multi_query($sql) === TRUE) {
+    echo "New records created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// query
+$sql = "SELECT username, password FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+	while($row = $result->fetch_assoc()) {
+		echo "id: " . $row["id"]. " - User: " . $row["username"]. " Password: " . $row["password"]. "<br>";
+	}
+} else {
+	echo "0 results";
+}
+
+
 $rs->close();
 $connection->close();
 ?>
