2f6abce9f136e006577ff25b5c1350c097bed387
Mehedi Hasan Masum <Mehedi Hasan Masum>
2016-07-08 13:57:04 +0600

    db debug

--- dbtest.php
+++ dbtest.php
@@ -1,9 +1,20 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
+echo $dbhost;
+echo "<br>";
 $dbport = getenv("MYSQL_SERVICE_PORT");
+echo $dbport;
+echo "<br>";
 $dbuser = getenv("MYSQL_USER");
+echo $dbuser;
+echo "<br>";
 $dbpwd = getenv("MYSQL_PASSWORD");
+echo $dbpwd;
+echo "<br>";
 $dbname = getenv("MYSQL_DATABASE");
+echo $dbname;
+echo "<br>";
+
 $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
