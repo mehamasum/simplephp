e6919c7d0348b01608bc4088f5bb919a6229b93a
mehamasum <mehamasum@gmail.com>
2016-08-24 01:16:07 +0600

    site test

--- index.php
+++ index.php
@@ -1,193 +1,84 @@
 <!DOCTYPE HTML>
+<!-- Website template by freewebsitetemplates.com -->
 <html>
-	<head>
-		<title>HealthPal Web</title>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-	</head>
-	<body class="homepage">
-
-		<!-- Header -->
-			<div id="header">
-				<div class="container">
-						
-					<!-- Logo -->
-						<h1><a href="#" id="logo">HealthPal Web</a></h1>
-					
-					<!-- Nav -->
-						<nav id="nav">
-							<ul>
-								<li><a href="index.php">Home</a></li>
-								<li><a href="left-sidebar.html">Paients</a></li>
-								<li><a href="right-sidebar.html">Account</a></li>
-								<li><a href="no-sidebar.html">About</a></li>
-							</ul>
-						</nav>
-
-
-					<!-- Banner -->
-						<div id="banner">
-							<div class="container">
-								<section>
-									<header class="major">
-										<h2>Welcome to HealthPal Web 2.1!</h2>
-										<span>A great way for physicans to keep in touch with patients.</span>
-									</header>
-									<a href="#" class="button alt">Log In</a>
-								</section>			
-							</div>
-						</div>
-
-				</div>
+<head>
+	<meta charset="UTF-8">
+	<title>HealthPal Web</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+	<div id="header">
+		<div>
+			<div class="logo">
+				<a href="index.php">HealthPal</a>
 			</div>
-
-		<!-- Featured -->
-			<div class="wrapper style2">
-				<section class="container">
-					<header class="major">
-						<h2>New set of features</h2>
-						<span class="byline">For your ease!</span>
-					</header>
-					<div class="row no-collapse-1">
-						<section class="4u">
-							<a href="#" class="image feature"><img src="images/pic02.jpg" alt=""></a>
-							<p>Patient vitals at your hand</p>
-						</section>
-						<section class="4u">
-							<a href="#" class="image feature"><img src="images/pic03.jpg" alt=""></a>
-							<p>Sync with cloud</p>
-						</section>
-						<section class="4u">
-							<a href="#" class="image feature"><img src="images/pic04.jpg" alt=""></a>
-							<p>Online appointments</p>
-						</section>
-	
-					</div>
-				</section>
+			<ul id="navigation">
+				<li class="active">
+					<a href="index.php">Home</a>
+				</li>
+				<li>
+					<a href="features.html">Features</a>
+				</li>
+				<li>
+					<a href="news.html">News</a>
+				</li>
+				<li>
+					<a href="about.html">About</a>
+				</li>
+				<li>
+					<a href="contact.html">Contact</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div id="adbox">
+		<div class="clearfix">
+			<img src="images/box.png" alt="Img" height="342" width="368">
+			<div>
+				<h1>Ideas?</h1>
+				<h2>That's what we live for.</h2>
+				<p>
+					Wix is an online website builder with a simple drag & drop interface, meaning you do the work online and instantly publish to the web. <span><a href="index.php" class="btn">Try It Now!</a><b>Don’t worry it’s for free</b></span>
+				</p>
 			</div>
-
-		<!-- Main -->
-			<div id="main" class="wrapper style1">
-				<section class="container">
-					<header class="major">
-						<h2>Nulla luctus eleifend</h2>
-						<span class="byline">Mauris vulputate dolor sit amet nibh</span>
-					</header>
-					<div class="row">
-					
-						<!-- Content -->
-							<div class="6u">
-								<section>
-									<ul class="style">
-										<li>
-											<span class="fa fa-wrench"></span>
-											<h3>Integer ultrices</h3>
-											<span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim.</span>
-										</li>
-										<li>
-											<span class="fa fa-cloud"></span>
-											<h3>Aliquam luctus</h3>
-											<span>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span>
-										</li>
-									</ul>
-								</section>
-							</div>
-							<div class="6u">
-								<section>
-									<ul class="style">
-										<li>
-											<span class="fa fa-cogs"></span>
-											<h3>Integer ultrices</h3>
-											<span>In posuere eleifend odio. Quisque semper augue mattis wisi. Maecenas ligula. Pellentesque viverra vulputate enim.</span>
-										</li>
-										<li>
-											<span class="fa fa-leaf"></span>
-											<h3>Aliquam luctus</h3>
-											<span>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Maecenas condimentum enim tincidunt risus accumsan.</span>
-										</li>
-									</ul>
-								</section>
-							</div>
-
-					</div>
-				</section>
+		</div>
+	</div>
+	<div id="contents">
+		<div id="tagline" class="clearfix">
+			<h1>Design With Simplicity.</h1>
+			<div>
+				<p>
+					You can replace all this text with your own text. Want an easier solution for a Free Website?
+				</p>
+				<p>
+					Head straight to Wix and immediately start customizing your website!
+				</p>
+				<p>
+					Wix is an online website builder with a simple drag & drop interface, meaning you do the work online and instantly publish to the web.
+				</p>
 			</div>
-
-		<!-- Footer -->
-			<div id="footer">
-				<div class="container">
-
-					<!-- Lists -->
-						<div class="row">
-							<div class="8u">
-								<section>
-									<header class="major">
-										<h2>Donec dictum metus</h2>
-										<span class="byline">Quisque semper augue mattis wisi maecenas ligula</span>
-									</header>
-									<div class="row">
-										<section class="6u">
-											<ul class="default">
-												<li><a href="#">Pellentesque elit non gravida blandit.</a></li>
-												<li><a href="#">Lorem ipsum dolor consectetuer elit.</a></li>
-												<li><a href="#">Phasellus nibh pellentesque congue.</a></li>
-												<li><a href="#">Cras vitae metus aliquam  pharetra.</a></li>
-											</ul>
-										</section>
-										<section class="6u">
-											<ul class="default">
-												<li><a href="#">Pellentesque elit non gravida blandit.</a></li>
-												<li><a href="#">Lorem ipsum dolor consectetuer elit.</a></li>
-												<li><a href="#">Phasellus nibh pellentesque congue.</a></li>
-												<li><a href="#">Cras vitae metus aliquam  pharetra.</a></li>
-											</ul>
-										</section>
-									</div>
-								</section>
-							</div>
-							<div class="4u">
-								<section>
-									<header class="major">
-										<h2>Donec dictum metus</h2>
-										<span class="byline">Mattis wisi maecenas ligula</span>
-									</header>
-									<ul class="contact">
-										<li>
-											<span class="address">Address</span>
-											<span>1234 Somewhere Road #4285 <br />Nashville, TN 00000</span>
-										</li>
-										<li>
-											<span class="mail">Mail</span>
-											<span><a href="#">someone@untitled.tld</a></span>
-										</li>
-										<li>
-											<span class="phone">Phone</span>
-											<span>(000) 000-0000</span>
-										</li>
-									</ul>	
-								</section>
-							</div>
-						</div>
-
-					<!-- Copyright -->
-						<div class="copyright">
-							Copyright 2016, BATFIA
-						</div>
-
-				</div>
+			<div>
+				<p>
+					You can replace all this text with your own text. Want an easier solution for a Free Website?
+				</p>
+				<p>
+					Head straight to Wix and immediately start customizing your website!
+				</p>
+				<p>
+					Wix is an online website builder with a simple drag & drop interface, meaning you do the work online and instantly publish to the web.
+				</p>
 			</div>
-
-	</body>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="clearfix">
+			<div id="connect">
+				<a href="http://freewebsitetemplates.com/go/facebook/" target="_blank" class="facebook"></a><a href="http://freewebsitetemplates.com/go/googleplus/" target="_blank" class="googleplus"></a><a href="http://freewebsitetemplates.com/go/twitter/" target="_blank" class="twitter"></a><a href="http://www.freewebsitetemplates.com/misc/contact/" target="_blank" class="tumbler"></a>
+			</div>
+			<p>
+				© 2023 Zerotype. All Rights Reserved.
+			</p>
+		</div>
+	</div>
+</body>
 </html>
