c4a2f9ba9b8a9ed5dfdb86bf79f55a0a72c665f2
mehamasum <mehamasum@gmail.com>
2016-08-27 01:54:19 +0600

    db and graph

--- graph/graph.php
+++ graph/graph.php
@@ -5,6 +5,33 @@
  include("../class/pData.class.php");
  include("../class/pDraw.class.php");
  include("../class/pImage.class.php");
+ 
+ 
+ // mysql -u system -p system -h 172.30.137.179 sampledb
+
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+echo $dbhost;
+echo "<br>";
+$dbport = getenv("MYSQL_SERVICE_PORT");
+echo $dbport;
+echo "<br>";
+$dbuser = getenv("MYSQL_USER");
+echo $dbuser;
+echo "<br>";
+$dbpwd = getenv("MYSQL_PASSWORD");
+echo $dbpwd;
+echo "<br>";
+$dbname = getenv("MYSQL_DATABASE");
+echo $dbname;
+echo "<br>";
+
+$connection = new mysqli($dbhost, "system", "system", "sampledb");
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+echo "<h1>My awesome DB test</h1>";
+
 
  /* Create and populate the pData object */
  $MyData = new pData();  
