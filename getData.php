4e408aac727a18395810e83ebcf2b2225ddd300e
mehamasum <mehamasum@gmail.com>
2016-08-27 03:23:30 +0600

    graph google

--- getData.php
+++ getData.php
@@ -1,42 +1,11 @@
-<?php
+<?php 
 
-// mysql -u system -p system -h 172.30.137.179 sampledb
+// This is just an example of reading server side data and sending it to the client.
+// It reads a json formatted text file and outputs it.
 
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-echo $dbhost;
-echo "<br>";
-$dbport = getenv("MYSQL_SERVICE_PORT");
-echo $dbport;
-echo "<br>";
-$dbuser = getenv("MYSQL_USER");
-echo $dbuser;
-echo "<br>";
-$dbpwd = getenv("MYSQL_PASSWORD");
-echo $dbpwd;
-echo "<br>";
-$dbname = getenv("MYSQL_DATABASE");
-echo $dbname;
-echo "<br>";
+$string = file_get_contents("data.json");
+echo $string;
 
-$connection = new mysqli($dbhost, "system", "system", "sampledb");
-if ($connection->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
+// Instead you can query your database and parse into JSON etc etc
 
-// query
-$sql = "SELECT humidity FROM measures";
-$result = $connection->query($sql);
-
-//create an array
-$emparray = array();
-while($row =mysqli_fetch_assoc($result))
-{
-	$emparray[] = $row;
-	echo $row. "<br>";
-}
-
-echo json_encode($emparray);
-
-$connection->close();
-?>
+?> 
