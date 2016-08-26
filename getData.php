8217116f19e84bd0723f5d06d25cb775e8be9193
mehamasum <mehamasum@gmail.com>
2016-08-27 02:46:39 +0600

    graph google

--- getData.php
+++ getData.php
@@ -9,3 +9,46 @@ echo $string;
 // Instead you can query your database and parse into JSON etc etc
 
 ?> 
+
+
+<?php
+
+// mysql -u system -p system -h 172.30.137.179 sampledb
+
+$dbhost = getenv("MYSQL_SERVICE_HOST");
+echo $dbhost;
+echo "<br>";
+$dbport = getenv("MYSQL_SERVICE_PORT");
+echo $dbport;
+echo "<br>";
+$dbuser = getenv("MYSQL_USER");
+echo $dbuser;
+echo "<br>";
+$dbpwd = getenv("MYSQL_PASSWORD");
+echo $dbpwd;
+echo "<br>";
+$dbname = getenv("MYSQL_DATABASE");
+echo $dbname;
+echo "<br>";
+
+$connection = new mysqli($dbhost, "system", "system", "sampledb");
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+// query
+$sql = "SELECT humidity FROM measures";
+$result = $connection->query($sql);
+
+//create an array
+$emparray = array();
+while($row =mysqli_fetch_assoc($result))
+{
+	$emparray[] = $row;
+}
+
+echo json_encode($emparray);
+
+$connection->close();
+?>
