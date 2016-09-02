ccaf692d58b67dfb3058e648e6b31b25da8f40e9
mehamasum <mehamasum@gmail.com>
2016-09-03 02:23:29 +0600

    android signup test

--- android/user-signup.php
+++ android/user-signup.php
@@ -12,7 +12,7 @@
 	}
 	 
 	 // insert values
-	$sql = "INSERT INTO web_user_info (email, first_name, last_name, sex, dob, weight, height, weight_unit, height_unit, hardwork, password, blood) VALUES ('" . $_POST['email'] . "','" . $_POST['first_name'] . "','" . $_POST['last_name'] . "','" . $_POST['sex'] . "',". $_POST['dobirth'] . ", ".$_POST['weight'] . " , " . $_POST['height'] . " ,'" . $_POST['weight_unit'] . "','" . $_POST['height_unit'] . "', " .$_POST['hardwork'] . ", '" . $_POST['password'] . "','" . $_POST['blood'] . "');";
+	$sql = "INSERT INTO web_user_info (email, first_name, last_name, sex, dobirth, weight, height, weight_unit, height_unit, hardwork, password, blood) VALUES ('" . $_POST['email'] . "','" . $_POST['first_name'] . "','" . $_POST['last_name'] . "','" . $_POST['sex'] . "',". $_POST['dob'] . ", ".$_POST['weight'] . " , " . $_POST['height'] . " ,'" . $_POST['weight_unit'] . "','" . $_POST['height_unit'] . "', " .$_POST['hardwork'] . ", '" . $_POST['password'] . "','" . $_POST['blood'] . "');";
 	
 	if ($connection->query($sql) === TRUE) {
 		echo "New records created successfully<br>";
