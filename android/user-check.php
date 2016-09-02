4d49abbe84a9cf3eaae670f069d9ed6360ae4263
mehamasum <mehamasum@gmail.com>
2016-09-02 19:50:11 +0600

    android signup test

--- android/user-check.php
+++ android/user-check.php
@@ -8,7 +8,7 @@
  if($auth == '01521112085') {
 	require_once('db-connect.php');
 	// query
-	$sql = "SELECT * FROM android_users WHERE email='$email'";
+	$sql = "SELECT * FROM web_user_info WHERE email='$email'";
 	$result = $connection->query($sql);
 
 	if ($result->num_rows > 0) {
