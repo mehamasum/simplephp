d2709a555e49844cbf4c8ce2225540dc89be2c69
mehamasum <mehamasum@gmail.com>
2016-09-02 20:18:40 +0600

    android signup test

--- db-connect.php
+++ db-connect.php
@@ -3,11 +3,12 @@
 	// mysql -u system -p system -h 172.30.137.179 sampledb
 	
 	$dbhost = getenv("MYSQL_SERVICE_HOST");
+	echo $dbhost."<br>";
 	$dbport = getenv("MYSQL_SERVICE_PORT");
 
 	$connection = new mysqli($dbhost, "system", "system", "sampledb");
 	if ($connection->connect_errno) {
-		printf("Connect failed: %s\n", $mysqli->connect_error);
+		echo $mysqli->connect_error."<br>";
 		exit();
 	}
 ?>
