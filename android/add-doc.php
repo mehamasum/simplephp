9b433e025bdc598dcb7bcf394be1ee0f6578868b
mehamasum <mehamasum@gmail.com>
2016-09-10 15:27:47 +0600

    add doc

--- android/add-doc.php
+++ android/add-doc.php
@@ -12,30 +12,41 @@
 	 
 	require_once('db-connect.php');
 	
+	
+	$sql = "SELECT * FROM web_physician WHERE email='". $doc."'";
+	$result = $connection->query($sql);
+	if ($result->num_rows==0) { //no doc !
+		echo "0";
+		die();
+	}
+	
 	// query
-	$sql = "SELECT * FROM web_user_info WHERE email='". $email . "' AND password='". $password. "'";
+	$sql = "SELECT * FROM physician_patient WHERE email_doc='". $doc . "' AND email_patient='". $email. "'";
 	$result = $connection->query($sql);
 	
-	if ($result->num_rows > 0) {
-		
-		$row = $result->fetch_assoc();
-		
-		$reply = array('first_name'=>$row['first_name'],
-			'last_name'=>$row['last_name'],
-			'sex'=>$row['sex'],
-			'dob'=>$row['dob'],
-			'weight'=>$row['weight'],
-			'height'=>$row['height'],
-			'weight_unit'=>$row['weight_unit'],
-			'height_unit'=>$row['height_unit'],
-			'hardwork'=>$row['hardwork'],
-			'blood'=>$row['blood']);
+	if ($result->num_rows==0) { //add to the list
+		// query
+		$sql = "INSERT INTO physician_patient (email_doc, email_patient) VALUES ('$doc', '$email')";
+		$result = $connection->query($sql);
+		if ($connection->query($sql) == FALSE) {
+			echo "1";
+			die();
+		}
+	}
+	
+	// send the doc info
+	$sql = "SELECT * FROM web_physician WHERE email='". $doc."'";
+	$new_result = $connection->query($sql);
+	$row = $new_result->fetch_assoc();
+	
+	$reply = array('name'=>$row['name'],
+		'degree'=>$row['degree'],
+		'specialist'=>$row['specialist'],
+		'phone'=>$row['phone'],
+		'location'=>$row['location'],
+		'visit_hour'=>$row['visit_hour']);
 
-		echo json_encode($reply); 
-	}
-	else {
-		echo "0";
-	}
+	echo json_encode($reply); 
 	
  }
 	
--- timepicker.php
+++ timepicker.php
@@ -1,21 +1,34 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <link type="text/css" href="css/bootstrap.min.css" />
-        <link type="text/css" href="css/bootstrap-timepicker.min.css" />
-        <script type="text/javascript" src="js/jquery.min.js"></script>
-        <script type="text/javascript" src="js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="js/bootstrap-timepicker.min.js"></script>
-    </head>
-    <body>
-        <div class="input-group bootstrap-timepicker timepicker">
-            <input id="timepicker1" type="text" class="form-control input-small">
-            <span class="input-group-addon"><i class="glyphicon glyphicon-time"></i></span>
-        </div>
- 
-        <script type="text/javascript">
-            $('#timepicker1').timepicker();
-        </script>
-    </body>
-</html>
+  <head>
+    <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/css/bootstrap-combined.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" media="screen"
+     href="http://tarruda.github.com/bootstrap-datetimepicker/assets/css/bootstrap-datetimepicker.min.css">
+  </head>
+  <body>
+    <div id="datetimepicker" class="input-append date">
+      <input type="text"></input>
+      <span class="add-on">
+        <i data-time-icon="icon-time" data-date-icon="icon-calendar"></i>
+      </span>
+    </div>
+    <script type="text/javascript"
+     src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js">
+    </script> 
+    <script type="text/javascript"
+     src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js">
+    </script>
+    <script type="text/javascript"
+     src="http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.min.js">
+    </script>
+    <script type="text/javascript"
+     src="http://tarruda.github.com/bootstrap-datetimepicker/assets/js/bootstrap-datetimepicker.pt-BR.js">
+    </script>
+    <script type="text/javascript">
+      $('#datetimepicker').datetimepicker({
+        format: 'dd/MM/yyyy hh:mm:ss',
+        language: 'pt-BR'
+      });
+    </script>
+  </body>
+<html>
